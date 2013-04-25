b49ea768bdeff8fb514065c4aedc202e38aada3f
Adam Wead <amsterdamos@gmail.com>
2013-04-25 13:38:51 -0400

    Initial install

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'bootstrap-sass'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'compass-rails'
+  gem 'compass-susy-plugin', :require => 'susy'
+end
+
+group :development do
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatalogWireframes::Application.config.secret_token = 'ee49a636ff1217a1bffaffcb51d2135fe1d857966d7d69c3eef437fdb27bced895a052062e1536bc7f1c7aedd3654758ec7c5c8f51eba1e2ce96276bbf7a190f'
