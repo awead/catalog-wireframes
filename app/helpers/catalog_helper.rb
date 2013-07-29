aa1cee27afbd4773f60f3dadc97eaef3158aa187
Adam Wead <amsterdamos@gmail.com>
2013-07-29 10:28:44 -0400

    frame navigation helpers

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,2 +1,12 @@
 module CatalogHelper
+
+  def next_frame frame
+    link_to "next", (catalog_index_path + "/" + frame), {:id => "next-wireframe"}
+  end
+
+  def previous_frame frame
+    link_to "previous", (catalog_index_path + "/" + frame), {:id => "previous-wireframe"}
+  end
+
+
 end
